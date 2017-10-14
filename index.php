3b4881021c4215d12abaac4ae4176c1d8d0beb91
Arun Basnet <z.arunbasnet@gmail.com>
2017-10-14 14:11:48 +0200

    Commit index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>GETTING STARTED</title>
+        <meta name="description" content="API docs for Hackathon2017Project">
+        
+    </head>
+    <body>
+        
+        <h1>Only API access available for the moment.</h1>
+        <h2>This is gonna be the docs!</h2>
+        <p>Please check back again later.</p>
+        
+    </body>
+</html>
